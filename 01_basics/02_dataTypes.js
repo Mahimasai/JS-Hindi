86a56c5c86ea479ea7a155e2b8b8a837cb27b9d8
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-02 11:17:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS Code as newer version
+
+//alert(3+3) // we are using nodejs not broweser
+
+console.log(3 + 3);
+console.log("hitesh");
+
+let name= "hitesh"
+let age= 18;
+let isLoggedIn= false;
+
+//number => 2 t  pw 52
+//bigint
+//string=>" "
+//boolean =>true/false
+//null stand alone value 
+//undefined =>
+//symbol
+
+
+//object
+
+console.log(typeof undefined);//undefined //shift + alt+ drpdwn
+console.log(typeof null); // object
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
-console.log("sai");
+//I want to build 
+const accountId = 14445553
+let accountEmail="hirgegeg@gmail.com"
+var accountPassword="123456"
+accountCity="jaipur"
+
+// accountId=2 // not allowed 
+
+
+accountEmail="fwhvfeiwv@gmail.com"
+accountPassword="5579797"
+accountCity="hyderabad"
+let accountState;
+
+
+/*
+Prefer not to use var because of issue in block and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+

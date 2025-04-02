690c613b638c37b9b9c2aa79ffc6686e3c261dfa
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-02 11:42:15 +0000

    convertions added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,23 +2,52 @@
 
 //alert(3+3) // we are using nodejs not broweser
 
-console.log(3 + 3);
-console.log("hitesh");
+// console.log(3 + 3);
+// console.log("hitesh");
 
-let name= "hitesh"
-let age= 18;
-let isLoggedIn= false;
+// let name= "hitesh"
+// let age= 18;
+// let isLoggedIn= false;
 
-//number => 2 t  pw 52
-//bigint
-//string=>" "
-//boolean =>true/false
-//null stand alone value 
-//undefined =>
-//symbol
+// //number => 2 t  pw 52
+// //bigint
+// //string=>" "
+// //boolean =>true/false
+// //null stand alone value 
+// //undefined =>
+// //symbol
 
 
-//object
+// //object
 
-console.log(typeof undefined);//undefined //shift + alt+ drpdwn
-console.log(typeof null); // object
+// console.log(typeof undefined);//undefined //shift + alt+ drpdwn
+// console.log(typeof null); // object
+
+// let score= "33";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc"=> NaN
+// true =>1 ; false => 0;
+
+// let islogged = " ";
+
+// let boolIsLogged= Boolean(islogged);
+// console.log(boolIsLogged);
+// //1 => true
+// //2 => false 
+// //"" => false
+// //" not empty string"=>true
+
+// let someNumber =33;
+
+// let StringNumber =  String(someNumber);
+// console.log(typeof StringNumber);
+// console.log(StringNumber)
+
+//num => string will be done 

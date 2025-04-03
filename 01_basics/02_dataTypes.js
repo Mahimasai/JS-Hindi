af209a0f8870b6fd410e8131aefc3d5732870417
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-03 07:30:05 +0000

    added comaprision

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -14,7 +14,7 @@
 // //string=>" "
 // //boolean =>true/false
 // //null stand alone value 
-// //undefined =>
+// //undefined =>---
 // //symbol
 
 
@@ -51,3 +51,55 @@
 // console.log(StringNumber)
 
 //num => string will be done 
+
+// let value = 3;
+// let negval = -value;
+// console.log(negval);
+
+// let str1= 'hello';
+// let str2='hitesh';
+
+// let dtr3=str1+str2;
+// console.log(str1+str2);
+// console.log(1+"2");
+// console.log("1"+2 + 2);//output 122
+// console.log(1+2+"2");//output 32
+// //these are just guidelines by ecma script
+
+// // console.log(3+4 * 5 % 3); practically yeh nahi hoga 
+// console.log(true);
+// console.log(+true);//outputs 1
+// console.log(+"");//outputs 0 because "" are 0
+// console.log(true+)//gives an error
+
+// let num2,num1,num3;
+// num1=num3=num2=2+2;
+
+// let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+
+// console.log(2>1); // true
+
+// console.log("2">1); //it dirctlyconvets to number
+// console.log("02">1);//they should be of same datayp type
+
+// console.log(null >0 );//falsse
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+// //the reason is that an eqality check == and comparision ><>= <= work differently beause 
+// //comaprision convert null to a number ,treating it as 0.
+// //That's why (3) null>=0 is true and (1) null>0 is false 
+
+// console.log(undefined == 0);//hamesha false dega 
+// console.log(undefined > 0);//hamesha false dega
+// console.log(undefined < 0);//hamesha false dega
+
+//Strict check
+
+// console.log("2"==2);//true
+// console.log("2"===2);//false
+
+
+
+

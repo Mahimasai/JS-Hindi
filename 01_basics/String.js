59c9e467d1d0df28717f2243ed0fdd9b3e1eaa77
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-04 05:48:23 +0000

    Strings-added

---
+++ 01_basics/String.js
@@ -0,0 +1,57 @@
+// //Strings in java 
+// //denoted by both single and double quotes
+// const name="hitesh"
+// const repoCount=50;
+// // console.log(name+repoCount+value)
+
+// console.log(`hello my name is ${name} and my repo name is ${repoCount}`)
+
+// const gameName= new String("Hitesh");
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+// //here the original name remains same because here string is non-primitive only the copy is passed primitives are arrays , object , functions 
+
+// //to see  position what is the character
+
+// console.log(gameName.charAt(2));
+
+// //to character is st which position
+
+// console.log(gameName.indexOf('t'));
+
+// //go through all the string once 
+// const name = "hitesh"
+// const repoCount = 50
+
+// // console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('hitesh-hc-com')
+
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));

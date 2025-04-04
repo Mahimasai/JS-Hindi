22f60fcdeea1a82e46cf45476e74c1123a7051c7
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-04 06:37:09 +0000

    mathNumbers

---
+++ 01_basics/Numbers_math.js
@@ -0,0 +1,40 @@
+// const score= 400;
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//it sets the precision
+// const otherNumber= 123.8888;
+// console.log(otherNumber.toPrecision(3))
+
+// const hundereds=1000000;
+// console.log(hundereds.toLocaleString('en-IN'));
+
+//+++++++++++++MATHS++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.5));//5
+// console.log(Math.round(4.5));//5
+// console.log(Math.ceil(4.3));//5
+// console.log(Math.floor(4.9));//4
+// console.log(Math.min(4,3,8,7));//3
+// //max is also there
+
+//MAth.randomn
+
+// console.log(Math.random());//values btw 0 and 1
+// //if i want values btw particular vales
+// console.log((Math.random()*10)+1);
+
+// const min=10;
+// const max=20;
+
+// console.log((Math.random()*(max - m+1))+min);
+
+
+
+
+
+

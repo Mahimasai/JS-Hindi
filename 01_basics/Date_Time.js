e3e3cbd9ed8158af06dabbc1a5ec4ca0ea596142
Mahimasai <saimahimanvitha2006@gmail.com>
2025-04-04 07:40:30 +0000

    DateTime

---
+++ 01_basics/Date_Time.js
@@ -0,0 +1,47 @@
+// // let myDate= new Date();
+// // console.log(myDate.toString());
+// // console.log(myDate.toISOString());
+// // console.log(myDate.toJSON());
+// // console.log(myDate.toLocaleTimeString());
+// // console.log(myDate.toLocaleString());
+// // console.log(myDate.toLocaleDateString());
+
+// // console.log(typeof myDate);//object
+
+// // let myCreatedDate= new Date(2023,0,23);// 23 january 2023
+// // console.log(myCreatedDate);
+// // console.log(myCreatedDate.toDateString());//here good formatt is seen
+// // console.log(myCreatedDate.toLocaleDateString());// 1/23/20203
+
+
+// // let mynewCreated= new Date(2025,0,4,12,29,30);
+// // console.log(myCreatedDate.toLocaleString());//date and time is printed
+// // console.log(myCreatedDate.toLocaleDateString());//only date is printed
+
+
+// // let mycreated= new Date("2023-01-14");//here month is jANUARY yy-mm-dd
+// // console.log(mycreated.toLocaleString());
+
+// // let newcrated=new Date("01-14-2025");//mm-dd-yy
+// // console.log(newcrated.toLocaleString());
+// let myCreatedDate=new Date("01-14-2023");
+
+
+// let myStamp= Date.now();
+// // console.log(myStamp);
+// // console.log(myCreatedDate.getTime());
+
+// console.log(Date.now());//in msec
+// console.log(Date.now()/1000);//in sec
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());//month starts from zero 
+
+// console.log(newDate.getDay());
+// console.log(`todays date is ${newDate}`)
+
+
+// newDate.toLocaleString('default',{ weekday: "long"});
+// console.log(newDate.toLocaleString('default',{ weekday: "long",
+//              timeZone: //search in google
+// }));
